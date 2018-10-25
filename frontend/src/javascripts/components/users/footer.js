423b4610dcaf4cafe5b522ca944db853cdda6a47
yuki-s-utmafia <y.shirota.research.a.develop@gmail.com>
2018-10-25 23:39:33 +0900

    refactoring renderLisr and modify stylesheets for footer

--- frontend/src/javascripts/components/users/footer.js
+++ frontend/src/javascripts/components/users/footer.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react'
 import ImgHolderOpen from '../../../images/main/planet_holder_btn.png'
+import { PlanetImgs } from './planet-img'
+import { DeleteIcons } from './delete-icon'
 
 class Footer extends Component {
+  componentDidMount() {
+    var planet_list = document.getElementById("planet-list");
+    planet_list.style.display = 'none';
+  }
+
+  onClickOpenPlanetHolder(event) {
+    var target_class = document.getElementsByClassName("open-planet-holder");
+    var planet_holder = document.getElementById("planet-holder");
+    var planet_list = document.getElementById("planet-list");
+
+    // メニュー表示/非表示
+    if(target_class[0].classList.contains("click-rotate")) {
+       target_class[0].classList.remove("click-rotate");
+       planet_list.classList.remove("is-show");
+       planet_list.style.display = 'none';
+       //planet_holder.classList.remove('holder-border');
+    } else {
+      target_class[0].classList.add("click-rotate");
+      planet_list.classList.add("is-show");
+      planet_list.style.display = '';
+      planet_list.style.width = '400px';
+      //planet_holder.classList.add('holder-border');
+    }
+  }
+
+  renderPlanetImgList(planetImg) {
+    return (
+      <li key={planetImg} className="planet" draggable="true"><img src={planetImg} className="planet-img"/></li>
+    )
+  }
+
+  renderDeleteIconList(deleteIcon) {
+    return (
+      <li key={deleteIcon} className="disapperance"><img src={deleteIcon} className="delete-btn"/></li>
+    )
+  }
+
   render() {
     return(
       <div id="footer">
         <div id="planet-holder">
-          <div className="open-planet circle-border">
-            <img src={ImgHolderOpen} />
+          <div className="open-planet-holder" onClick={this.onClickOpenPlanetHolder.bind(this)}>
+            <img src={ImgHolderOpen} className="planet-holder-img"/>
           </div>
           <ul id="planet-list">
-            <li id="earth" className="planet" draggable="true">地球</li>
-            <li className="planet" draggable="true">YO</li>
-            <li className="planet" draggable="true">OH</li>
-            <li className="planet" draggable="true">OH</li>
-            <li className="planet" draggable="true">OH</li>
-            <li className="planet" draggable="true">OH</li>
-            <li className="planet" draggable="true">OH</li>
+            { PlanetImgs.map(this.renderPlanetImgList) }
           </ul>
         </div>
         <div id="disapperance-holder">
           <ul id="disapperance-list">
-            <li className="disapperance" id="pause">HI</li>
-            <li className="disapperance" id="restart">HI</li>
-            <li className="disapperance">HI</li>
+            { DeleteIcons.map(this.renderDeleteIconList) }
           </ul>
         </div>
       </div>
