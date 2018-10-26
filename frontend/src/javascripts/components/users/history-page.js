367e5739620405c0b3fc2067f03297880eab5f64
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-10-26 09:12:52 +0900

    Create access to history page (#21)

---
+++ frontend/src/javascripts/components/users/history-page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class HistoryPage extends Component {
+  render() {
+    return(
+      <div>History</div>
+    )
+  }
+}
+
+export default connect()(HistoryPage)
