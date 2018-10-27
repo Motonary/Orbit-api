960fdcd6afb147c3f13c52b9c2ec5f3bbe5cb24b
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-10-27 16:08:06 +0900

    Create basic update form (#34)

--- frontend/src/javascripts/components/users/profile-page.js
+++ frontend/src/javascripts/components/users/profile-page.js
@@ -1,9 +1,78 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
 
-class ProfilePage extends Component {
+class ProfileUpdateForm extends Component {
+  renderTextField({ placeholder, type, input, meta: { touched, error } }) {
+    const classNames = `form-group ${touched && error ? 'has-danger' : ''} field-style fieled-text`
+    return(
+      <div className={classNames}>
+        <input
+          className="text-style"
+          placeholder={placeholder}
+          type={type}
+          {...input} />
+        <div className="text-help">
+          {touched ? error: ''}
+        </div>
+      </div>
+    )
+  }
+
+  renderImageField({ type }) {
+    return(
+      <input type={type}/>
+    )
+  }
+
+  onSubmit({ username, email, password, confirmation }) {
+    this.props.createUser(username, email, password, confirmation, (newUserId) => {
+      this.props.history.push(`/users/${newUserId}`)
+    })
+  }
+
   render() {
-    return <div>Hello, ProfilePage!</div>
+    return(
+      <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))} className="signup-form">
+        <Field name="avatar" type="file" component={this.renderImageField} />
+        <Field placeholder="NAME" name="username" type="text" component={this.renderTextField} />
+        <Field placeholder="EMAIL ADRESS" name="email" type="text" component={this.renderTextField} />
+        <Field placeholder="PASSWORD" name="password" type="password" component={this.renderTextField} />
+        <Field placeholder="CONFIRM PASSWORD" name="confirmation" type="password" component={this.renderTextField} />
+        <button type="submit" className="submit-btn">SIGN UP</button>
+      </form>
+    )
   }
 }
 
-export default ProfilePage
+function validate(values) {
+  const errors = {}
+
+  if (values.username && values.username.length > 50) {
+    errors.username = "Too long username"
+  }
+
+  if (values.email && values.email.length > 255) {
+    errors.email = "Too long email address"
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = "Password required"
+  } else if (values.password.length < 6) {
+    errors.password = "Password must contain at least 6 characters"
+  }
+
+  if (!values.confirmation) {
+    errors.confirmation = "Password confirmation required"
+  } else if (values.password !== values.confirmation) {
+    errors.confirmation = "Not match password"
+  }
+  return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'ProfileUpdateForm'
+})(connect()(ProfileUpdateForm))
