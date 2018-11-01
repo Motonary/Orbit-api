f7acad1bd4ef003d95a4ac558fba32ba83249d0d
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-11-01 17:23:54 +0900

    Test-deploy-3

--- db/migrate/20181027023702_change_enum_column_to_string.rb
+++ db/migrate/20181027023702_change_enum_column_to_string.rb
@@ -1,6 +1,5 @@
 class ChangeEnumColumnToString < ActiveRecord::Migration[5.2]
   def change
-    change_column :projects, :fixed_star_type, :string
     change_column :assignments, :planet_type, :string
     change_column :assignments, :planet_size, :string
     change_column :assignments, :orbit_pos, :string
--- db/migrate/20181027032102_rollback_enum_string_columns.rb
+++ db/migrate/20181027032102_rollback_enum_string_columns.rb
@@ -1,6 +1,5 @@
 class RollbackEnumStringColumns < ActiveRecord::Migration[5.2]
   def change
-    change_column :projects, :fixed_star_type, :integer
     change_column :assignments, :planet_type, :integer
     change_column :assignments, :planet_size, :integer
     change_column :assignments, :orbit_pos, :integer
