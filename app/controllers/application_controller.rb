481ffa880351c8010eda46b5facd1fab7edd5e51
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-11-02 08:08:12 +0900

    test-deploy-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   def fallback_index_html
-    render :file => '/frontend/dist/index.html'
+    render :file => 'frontend/dist/index.html'
   end
 end
