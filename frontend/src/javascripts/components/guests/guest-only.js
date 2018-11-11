b75b4ce59ace3aea87a05a49ddd9d51678fb9765
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-11-12 07:56:27 +0900

    Complete top page layout

--- frontend/src/javascripts/components/guests/guest-only.js
+++ frontend/src/javascripts/components/guests/guest-only.js
@@ -5,14 +5,15 @@ import TopPage from './top-page'
 
 class GuestOnly extends Component {
   render() {
-    const { url } = this.props.match
+    // const { url } = this.props.match
     const { currentUser } = this.props
 
     return(
       !currentUser ? (
           <Switch>
-            <Route exact path={`${url}/signin`} component={TopPage} />
-            <Route exact path={`${url}/signup`} component={TopPage} />
+            {/*<Route exact path={`${url}/signin`} component={TopPage} />
+            <Route exact path={`${url}/signup`} component={TopPage} />*/}
+            <Route exact path="/guests" component={TopPage} />
             <Route render={() => <h2>404 Not Found</h2>} />
           </Switch>
       ) : <Redirect to={`/users/${currentUser.id}`} />
--- frontend/src/javascripts/components/guests/top-page.js
+++ frontend/src/javascripts/components/guests/top-page.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import SigninForm from './signin-form'
-import SignupForm from './signup-form'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
 import ImgLogo from '../../../images/index/logo.png'
 import ImgPlanet from '../../../images/index/top_earth.png'
 
-export default class TopPage extends Component {
+class TopPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSignIn: true,
+      isSignUp: false
+    }
+  }
+
+  renderField({ placeholder, type, input, meta: { touched, error } }) {
+    const classNames = `form-group ${touched && error ? 'has-danger' : ''} field-style fieled-text`
+    return(
+      <div className={classNames}>
+        <input
+          className="text-style"
+          placeholder={placeholder}
+          type={type}
+          {...input} />
+        <div className="text-help">
+          {touched ? error: ''}
+        </div>
+      </div>
+    )
+  }
+
+  onSubmit({ username, email, password, confirmation }) {
+    // this.props.createUser(username, email, password, confirmation, (newUserId) => {
+    //   this.props.history.push(`/users/${newUserId}`)
+    // })
+    console.log('Success!')
+  }
+
   render() {
+    const { isSignIn, isSignUp } = this.state
     return (
       <div className="top-page-container">
         <div className="logo-container">
           <img src={ImgLogo} className="top-page-logo" />
         </div>
         <div className="sign-form">
-          <Switch>
-            <Route exact path="/guests/signin" component={SigninForm} />
-            <Route exact path="/guests/signup" component={SignupForm} />
-          </Switch>
+          {(isSignIn && !isSignUp) ? (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+              <Field placeholder="EMAIL ADRESS" name="email" type="text" component={this.renderField} />
+              <Field placeholder="PASSWORD" name="password" type="password" component={this.renderField} />
+              <button type="submit" className="submit-btn">SIGN IN</button>
+            </form>
+          ) : (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))} className="signup-form">
+              <Field placeholder="NAME" name="username" type="text" component={this.renderField} />
+              <Field placeholder="EMAIL ADRESS" name="email" type="text" component={this.renderField} />
+              <Field placeholder="PASSWORD" name="password" type="password" component={this.renderField} />
+              <Field placeholder="CONFIRM PASSWORD" name="confirmation" type="password" component={this.renderField} />
+              <button type="submit" className="submit-btn">SIGN UP</button>
+            </form>
+          )}
         </div>
         <div className="planet-img-container">
           <img src={ImgPlanet} className="top-page-planet" />
@@ -25,3 +67,40 @@ export default class TopPage extends Component {
     )
   }
 }
+
+function validate(values) {
+  const errors = {}
+  // const { isSignIn, isSignUp } = this.state
+
+  if (!values.email) {
+    errors.email = "Email required"
+  } else if(values.email.length > 255) {
+    errors.email = "Too long email address"
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = "Password required"
+  } else if (values.password.length < 6) {
+    errors.password = "Password must contain at least 6 characters"
+  }
+
+  if (!values.username) {
+    errors.username = "Username required"
+  } else if (values.username.length > 50) {
+    errors.username = "Too long username"
+  }
+
+  if (!values.confirmation) {
+    errors.confirmation = "Password confirmation required"
+  } else if (values.password !== values.confirmation) {
+    errors.confirmation = "Not match password"
+  }
+  return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'TopPageForm'
+})(connect()(TopPage))
