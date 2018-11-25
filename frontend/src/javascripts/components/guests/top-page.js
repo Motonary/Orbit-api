20ef379ff313e0d2724915d92cb87eb5709b65b9
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-11-25 19:34:46 +0900

    Componentize top-page-logo

--- frontend/src/javascripts/components/guests/top-page.js
+++ frontend/src/javascripts/components/guests/top-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import TopPageButton from '../atoms/toppage-btn'
+import TopPageLogo from '../atoms/toppage-logo'
 import TopPageForm from '../molecules/forms/toppage-form'
-import ImgLogo from '../../../images/index/logo.png'
 import ImgPlanet from '../../../images/index/top_earth.png'
 
 export default class TopPage extends Component {
@@ -31,14 +31,13 @@ export default class TopPage extends Component {
   render() {
     const { isSignIn, isSignUp } = this.state
     const { history } = this.props
+
     return (
       <div className="top-page-container">
         <div onClick={this.onClickTopPageButton.bind(this)}>
           <TopPageButton isSignIn={isSignIn} />
         </div>
-        <div className="logo-container">
-          <img src={ImgLogo} className="top-page-logo" />
-        </div>
+        <TopPageLogo />
         <TopPageForm
           isSignIn={isSignIn}
           isSignUp={isSignUp}
