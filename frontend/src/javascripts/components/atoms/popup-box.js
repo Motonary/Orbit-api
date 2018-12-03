7dea1f9c91d5b2b1c722c659ac6e3735ddb17e07
yuki-s-utmafia <y.shirota.research.a.develop@gmail.com>
2018-12-03 18:36:18 +0900

    modify constants definition

--- frontend/src/javascripts/components/atoms/popup-box.js
+++ frontend/src/javascripts/components/atoms/popup-box.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-const PopupBox = assignmentInfo => {
-  const data = assignmentInfo.assignmentInfo
-  const deadline = data.deadline.split('T')[0].split('-')
+const PopupBox = ({ assignmentInfo }) => {
+  const deadline = assignmentInfo.deadline.split('T')[0].split('-')
+  const title = assignmentInfo.title
+  const detail = assignmentInfo.detail
 
   return (
     <div className="detail-balloon">
       <div className="assignment-data">
-        <div className="assignment-title">{data.title}</div>
+        <div className="assignment-title">{title}</div>
         <div className="assignment-deadline">
           {deadline[0]}年{deadline[1]}月{deadline[2]}日
         </div>
-        <div className="assignment-description">{data.detail}</div>
+        <div className="assignment-description">{detail}</div>
       </div>
     </div>
   )
