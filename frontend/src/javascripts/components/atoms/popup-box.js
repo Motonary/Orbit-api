e35f0026fdfb1bb7f61626b525fb70d9d64c74ff
yuki-s-utmafia <y.shirota.research.a.develop@gmail.com>
2018-12-03 16:06:41 +0900

    modify project page popup-box

--- frontend/src/javascripts/components/atoms/popup-box.js
+++ frontend/src/javascripts/components/atoms/popup-box.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 const PopupBox = assignmentInfo => {
   const data = assignmentInfo.assignmentInfo
+  const deadline = data.deadline.split('T')[0].split('-')
 
   return (
     <div className="detail-balloon">
-      <div className="assignment-title">{data.title}</div>
-      <span>{data.deadline}</span>
-      <div className="assignment-description">{data.detail}</div>
-      <div className="select-btn">選択</div>
+      <div className="assignment-data">
+        <div className="assignment-title">{data.title}</div>
+        <div className="assignment-deadline">
+          {deadline[0]}年{deadline[1]}月{deadline[2]}日
+        </div>
+        <div className="assignment-description">{data.detail}</div>
+      </div>
     </div>
   )
 }
