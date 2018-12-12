4a69d35e8432e9c446a42756e7f38a0a9551615e
yuki-s-utmafia <y.shirota.research.a.develop@gmail.com>
2018-12-13 05:03:17 +0900

    add mocks file and modify package.json

--- frontend/src/__tests__/atoms/planet-img.test.js
+++ frontend/src/__tests__/atoms/planet-img.test.js
@@ -5,7 +5,7 @@ import PlanetImg from '../../javascripts/components/atoms/planet-img'
 import { PlanetImgs } from '../../javascripts/constants/images'
 
 describe('<PlanetImg />', () => {
-  it('renders an planet image', () => {
+  it('correct props are handed over as rendering', () => {
     const planetImg = shallow(<PlanetImg src={PlanetImgs.Earth} />)
     expect(planetImg.find('img').prop('src') === PlanetImgs.Earth).toBe(true)
   })
