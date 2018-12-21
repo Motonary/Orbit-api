ab67745dc1e3360b2a1cf42720b28473a8d31843
rechtburg <y.shirota.research.a.develop@gmail.com>
2018-12-21 18:23:10 +0900

    add animation

--- frontend/src/javascripts/components/molecules/revival.tsx
+++ frontend/src/javascripts/components/molecules/revival.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import anime from 'animejs'
 
 import ActionBtn from '../atoms/buttons/action-btn'
 
-import { restoreAssignment } from '../../actions/assignments'
+import { restoreAssignment, resetSelectedAssignment } from '../../actions/assignments'
+import { resetDestroyAction } from '../../actions/common'
 
 interface RevivalProps {
   icon: string
@@ -11,10 +13,13 @@ interface RevivalProps {
   onClick: () => void
 
   selectedAssignments: any
-  selectedDestroyAction: any
-  modalOpen: any
 
+  modalOpen: any
+  selectedDestroyAction: any
+
+  resetSelectedAssignment: any
   restoreAssignment: any
+  resetDestroyAction: any
 }
 
 class Revival extends React.Component<RevivalProps, {}> {
@@ -22,13 +27,36 @@ class Revival extends React.Component<RevivalProps, {}> {
     if (this.props.selectedAssignments.length === 0) return
     if (this.props.modalOpen !== '') return
     if (this.props.selectedDestroyAction !== 'Revival') return
-    this.props.selectedAssignments.forEach((assignment: string) => {
-      this.callRestoreAssignment(assignment.split('-')[0])
-    })
+    this.iginiteRevivalAnimation()
   }
 
-  callRestoreAssignment(assignmentId: string) {
-    this.props.restoreAssignment(assignmentId)
+  iginiteRevivalAnimation() {
+    Promise.resolve()
+      .then(() => {
+        this.props.selectedAssignments.forEach((assignment: string) => {
+          this.removeImg(assignment)
+          this.props.restoreAssignment(assignment.split('-')[0])
+        })
+      })
+      .then(() => {
+        this.props.resetDestroyAction()
+        this.props.resetSelectedAssignment()
+      })
+  }
+  removeImg(assignmentId: string) {
+    anime({
+      targets: `#planet-${assignmentId} .stored-planet`,
+      rotate: {
+        value: '6turn',
+        duration: 1500,
+        easing: 'easeInExpo',
+      },
+      opacity: {
+        value: [1, 0],
+        duration: 1500,
+        easing: 'easeInExpo',
+      },
+    })
   }
 
   render() {
@@ -44,5 +72,5 @@ export default connect(
     selectedDestroyAction,
     modalOpen,
   }),
-  { restoreAssignment }
+  { restoreAssignment, resetSelectedAssignment, resetDestroyAction }
 )(Revival)
--- frontend/src/javascripts/components/organisms/history-page-main.tsx
+++ frontend/src/javascripts/components/organisms/history-page-main.tsx
@@ -1,29 +1,15 @@
 import * as React from 'react'
-// import { connect } from 'react-redux'
 
 import HistoryCanvas from '../molecules/history-canvas'
 import StoredPlanetList from '../molecules/stored-planet-list'
 
-// interface Props {
-//   destroyedAssignments: any
-//   fetchDestroyedAssignments: any
-//   restoreAssignment: any
-// }
-
-export default class HistoryPageMain extends React.Component<{}, {}> {
-  render() {
-    return (
-      <div id="history-container">
-        <HistoryCanvas />
-        <StoredPlanetList />
-        {/*<a onClick={this.onRestoreAssignment.bind(this, 7)}>RESTORE</a>*/}
-        {/*<div>{destroyedAssignments.map(this.renderDestroyedAssignments)}</div>*/}
-      </div>
-    )
-  }
+const HistoryPageMain: React.SFC<{}> = ({}) => {
+  return (
+    <div id="history-container">
+      <HistoryCanvas />
+      <StoredPlanetList />
+    </div>
+  )
 }
 
-// export default connect(
-//   ({ destroyedAssignments }: any) => ({ destroyedAssignments }),
-//   { fetchDestroyedAssignments, restoreAssignment }
-// )(HistoryPageMain)
+export default HistoryPageMain
