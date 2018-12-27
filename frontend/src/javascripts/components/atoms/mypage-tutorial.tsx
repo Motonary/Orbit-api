f825a77a27696fe27437578afd06d94f5f9fbaa9
rechtburg <y.shirota.research.a.develop@gmail.com>
2018-12-28 03:19:42 +0900

    modify style and uncommited file

---
+++ frontend/src/javascripts/components/atoms/mypage-tutorial.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const MyPageTutorial: React.SFC<{}> = () => {
+  return (
+    <ul className="tutorial-list">
+      <li>回っている惑星をクリックしてProjectPageへ遷移してみよう！</li>
+    </ul>
+  )
+}
+
+export default MyPageTutorial
---
+++ frontend/src/javascripts/components/atoms/projectpage-tutorial.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const ProjectPageTutorial: React.SFC<{}> = () => {
+  return (
+    <ul className="tutorial-list">
+      <li>左下のプラスボタンを押してみよう！</li>
+      <li>惑星をドラッグして軌道の上に移動させてみよう！</li>
+      <li>軌道が光ったらドロップしてみよう！</li>
+      <li>Assignment作成フォームを埋めたら自動で追加されるよ！</li>
+    </ul>
+  )
+}
+
+export default ProjectPageTutorial
