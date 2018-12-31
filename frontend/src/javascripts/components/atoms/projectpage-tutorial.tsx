32df2500d09f47e73fc2fb8dc203b174ac4c1210
rechtburg <y.shirota.research.a.develop@gmail.com>
2018-12-31 09:20:17 +0900

    modify tutorial list

--- frontend/src/javascripts/components/atoms/projectpage-tutorial.tsx
+++ frontend/src/javascripts/components/atoms/projectpage-tutorial.tsx
@@ -3,10 +3,11 @@ import * as React from 'react'
 const ProjectPageTutorial: React.SFC<{}> = () => {
   return (
     <ul className="tutorial-list">
-      <label>リストの削除</label>
+      <label className="label">リストの削除</label>
       <li>軌道上の惑星を１つクリックしてチェックマークを付けよう！</li>
       <li>そのまま右下の3つのボタンから好きなものをクリックしてみよう！</li>
-      <label>リストの追加</label>
+      <br />
+      <label className="label">リストの追加</label>
       <li>左下のプラスボタンを押してみよう！</li>
       <li>出てきた惑星をドラッグ＆ドロップして軌道上に追加しよう！</li>
     </ul>
